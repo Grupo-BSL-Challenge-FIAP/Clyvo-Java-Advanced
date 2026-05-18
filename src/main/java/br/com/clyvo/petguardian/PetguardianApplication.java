b6cdd334dd8b62ddcab1236a5b0e32c8edba4c42
manuelalacerda <manuelalacerda88@gmail.com>
2026-05-18 11:13:39 -0300

    chore adicionando cache no PetGuardianApplication

--- src/main/java/br/com/clyvo/petguardian/PetguardianApplication.java
+++ src/main/java/br/com/clyvo/petguardian/PetguardianApplication.java
@@ -2,8 +2,10 @@ package br.com.clyvo.petguardian;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class PetguardianApplication {
 
 	public static void main(String[] args) {
